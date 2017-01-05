88492e3a36fad2ceca1591b3d86bdeb5faed4f76
Joel Johnson <joelconradjohnson@gmail.com>
2017-01-04 19:35:35 -0600

    add fullscreen

--- main.py
+++ main.py
@@ -3,15 +3,19 @@
 import Tkinter as tk
 
 class Application(tk.Frame):
-        def __init__(self, master=None):
-                tk.Frame.__init__(self, master)
-                self.grid()
-                self.createWidgets()
+    def __init__(self, master):
 
-        def createWidgets(self):
-                self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-                self.quitButton.grid()
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
+        pad=3
+        master.attributes("-fullscreen", True)
 
-app = Application()
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text='Quit', command=self.quit)
+        self.quitButton.grid()
+
+root=tk.Tk()
+app = Application(root)
 app.master.title('Sample Application')
 app.mainloop()
