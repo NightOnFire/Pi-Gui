71054a988c5625146fa7127daf95b733206a9953
Joel Johnson <joelconradjohnson@gmail.com>
2017-01-10 17:50:00 -0600

    read file and make buttons

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import Tkinter as tk
+import json
+import logging
 
 class Application(tk.Frame):
     def __init__(self, master):
@@ -11,7 +13,20 @@ class Application(tk.Frame):
         pad=3
         master.attributes("-fullscreen", True)
 
+    def readJson(self):
+        json_data=open('apps.json').read()
+        self.apps = json.loads(json_data)
+
+    def runApp(self, path):
+        logging.warning(path)
+
     def createWidgets(self):
+        self.readJson()
+
+        for app in self.apps:
+            button = tk.Button(self, text=app.get("name"), command=lambda:self.run(app.get("name")))
+            button.grid()
+
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
         self.quitButton.grid()
 
